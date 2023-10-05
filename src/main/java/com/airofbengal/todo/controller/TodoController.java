59bb8844dd1e918398ec8db563cd6664dd822083
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2023-10-05 14:51:40 +0600

    Add method level security

--- src/main/java/com/airofbengal/todo/controller/TodoController.java
+++ src/main/java/com/airofbengal/todo/controller/TodoController.java
@@ -5,6 +5,7 @@ import com.airofbengal.todo.service.TodoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,40 +17,47 @@ import java.util.List;
 public class TodoController {
     private TodoService todoService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto){
         TodoDto savedTodoDto = todoService.addTodo(todoDto);
         return new ResponseEntity<>(savedTodoDto, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping("{id}")
     public ResponseEntity<TodoDto> getTodo(@PathVariable Long id){
         return ResponseEntity.ok(todoService.getTodo(id));
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @GetMapping
     public ResponseEntity<List<TodoDto>> getTodos(){
         return ResponseEntity.ok(todoService.getTodos());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("{id}")
     public ResponseEntity<TodoDto> updateTodo(@RequestBody TodoDto todoDto, @PathVariable("id") Long todoId){
         TodoDto updatedTodo = todoService.updateTodo(todoDto, todoId);
         return ResponseEntity.ok(updatedTodo);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteTodo(@PathVariable Long id){
         todoService.deleteTodo(id);
         return ResponseEntity.ok("Successfully deleted the todo with id: "+id);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @PatchMapping("{id}/complete")
     public ResponseEntity<TodoDto> completeTodo(@PathVariable Long id){
         TodoDto todoDto = todoService.completeTodo(id);
         return ResponseEntity.ok(todoDto);
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     @PatchMapping("{id}/incomplete")
     public ResponseEntity<TodoDto> inCompleteTodo(@PathVariable Long id){
         TodoDto todoDto = todoService.inCompleteTodo(id);
